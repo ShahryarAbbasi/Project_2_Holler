1a6b180bd3cd755038a66102bc853a19b27e9d92
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-08-29 14:47:07 -0400

    updates

--- models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    name: {type: String, required: true},
-});
-
-const user = mongoose.model('user', userSchema);
-module.exports = user;
