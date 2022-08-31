5699143ebe3414ce8eab925855ddc2b7ed0bd1a5
“philberdecio” <philberdecio@gmail.com>
2022-08-31 08:57:43 -0500

    test adding holler

--- dbTest.js
+++ dbTest.js
@@ -3,7 +3,22 @@ const mongoose = require("mongoose");
 
 const db = require('./models')
 
-Holler.create ({
-    body: "Hello World"
+async function createHoller(data){
+    try{
+
+        const newHoller = await db.Holler.create(data)
+        console.log(newHoller)
+
+    }catch(err){
+        throw new Error(err)
+    }
+}
+
+
+
+const newDoc = {
+    body: "Hello World",
     user: "630e3714cae5e4174e3c1517"
-})
+}
+
+createHoller(newDoc)
