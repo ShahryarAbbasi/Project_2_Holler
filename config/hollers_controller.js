6f99a7be2b91d935e1dbb69365998dcda37c2b98
Kingsley <kingsley550@yahoo.com>
2022-08-31 08:12:16 -0500

    Refine holler routes

--- config/hollers_controller.js
+++ config/hollers_controller.js
@@ -1,27 +1,76 @@
 const express = require("express");
 const router = express.Router();
+
+// Middleware that might be needed later on
+// router.use(express.json());
+// router.use(express.urlencoded({extended:false}));
+
+const db = require("../models");
 // All hollers
 router.get("/", (req, res) => {
   res.send("All the hollers right here");
 });
 // getting the form for a new holler
 router.get("/new", (req, res) => {
-  res.send("holler  New Form ");
+  res.send("New holler ");
 });
+
 // creating a new holler
-router.post("/", (req, res) => {
-  res.send("create holler");
+router.post("/", async (req, res) => {
+  const createdHoller = req.body;
+  try {
+    const newHoller = await db.Holler.create(createdHoller);
+
+    res.redirect(`/hollers`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
 });
+
 // show a specific holler
-router.get("/:id", (req, res) => {
-  res.send("holler get ");
+router.get("/:hollerIndex", async (req, res) => {
+  try {
+    const foundHoller = await db.Hollers.findById(req.params.hollerIndex);
+    res.render("show.ejs", { holler: foundHoller, id: foundHoller._id });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
+  // res.send(`Show holler ${req.params.id} `);
 });
-// editing info on a holler
-router.put("/:id", (req, res) => {
-  res.send("edit holler");
+
+// post request for all hollers from holler db
+router.get("/", async (req, res) => {
+  try {
+    const allHollers = await db.Hollerss.find();
+    const context = { products: allHollers };
+    res.render("index.ejs", context);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
 });
+
 // deleting a holler
-router.delete("/:id", (req, res) => {
-  res.send("delete holler");
+router.delete("/:hollerId", async (req, res) => {
+  try {
+    const foundHoller = await db.Hollers.findByIdAndDelete(req.params.hollerId);
+    return res.redirect("/hollers");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
+});
+
+// editing a holler
+router.get("/:hollerId/edit", async (req, res) => {
+  try {
+    const foundHoller = await db.Hollers.findById(req.params.hollerId);
+    res.render("edit.ejs", { holler: foundHoller, id: foundHoller._id });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
 });
 module.exports = router;
